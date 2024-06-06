320b1f6779f3b4569e8eb7692095c1b403afb1af
alirezashon <alirezafeshki2017@gmail.com>
2024-06-06 14:57:03 +0330

    set inline styles in store

--- src/Components/Store/index.tsx
+++ src/Components/Store/index.tsx
@@ -2,21 +2,34 @@ import styles from "./index.module.css"
 import { BiSearch } from "react-icons/bi"
 import { MdAddCircle, MdDeleteForever } from "react-icons/md"
 import { IoIosArrowForward } from "react-icons/io"
-import { useRef, useState } from "react"
+import { useRef, useState, RefObject, useEffect } from "react"
 import { Product, Category } from "@/DTO"
 import Image from "next/image"
 import { FaMinus } from "react-icons/fa"
 import { Add, Get, Remove } from "../Basket/Actions"
 import { useBasket } from "@/Context"
+import { FaBasketShopping } from "react-icons/fa6"
+import { GiCrossMark } from "react-icons/gi"
 
 interface Props {
   data: [Category[], Product[]]
 }
 const Store: React.FC<Props> = ({ data }) => {
+  const refs: {
+    [key: string]: RefObject<HTMLInputElement | HTMLTextAreaElement>
+  } = {
+    search: useRef<HTMLInputElement>(null),
+  }
   const { basket, setBasket } = useBasket()
+  const [isMobile, setIsMobile] = useState<boolean>(true)
+  const [isBasketOpen, setIsBasketOpen] = useState<boolean>(true)
   const categoryBoxRef = useRef<HTMLDivElement>(null)
   const [showProducto, setShowProducto] = useState<string>("")
   const [productover, setProductover] = useState<number | null>(null)
+
+  useEffect(() => {
+    innerWidth > 878 && setIsMobile(false)
+  }, [])
   const scrollLeft = () => {
     if (categoryBoxRef.current) {
       categoryBoxRef.current.scrollBy({ left: -100, behavior: "smooth" })
@@ -44,32 +57,50 @@ const Store: React.FC<Props> = ({ data }) => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.basketSide}>
-          <div className={styles.basketHead}>
-            <MdDeleteForever className={styles.garbage} />
-            <div>
-              سبد خرید
-              {`(${count})`}
+        {isBasketOpen && (
+          <div className={styles.basketSide}>
+            <div className={styles.basketHead}>
+              <div>
+                سبد خرید
+                {`(${count})`}
+              </div>
+              <GiCrossMark
+                className={styles.garbage}
+                onClick={() => setIsBasketOpen(false)}
+              />
             </div>
+            {basket.map((product) => (
+              <div className={styles.basketController}>
+                <div className={styles.titlePrice}>
+                  <p>
+                    {/* {data[1][data[1].findIndex((d) => d._id === product)]?.title} */}
+                  </p>
+                  <MdDeleteForever className={styles.garbage} />
+
+                  <p>
+                    {`${totalPrice}`.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    تومان
+                  </p>
+                </div>
+                <div className={styles.priceaction}>
+                  <MdAddCircle className={styles.inceriment} size={"3vh"} />
+                  <p className={styles.total}>{count}</p>
+                  <FaMinus className={styles.deceriment} size={"3vh"} />
+                </div>
+              </div>
+            ))}
+            <div className={styles.basketDetail}></div>
           </div>
-          <div className={styles.basketController}>
-            <div className='titlePrice'>
-              <p>{title}</p>
-              <p>{totalPrice} تومان</p>
-            </div>
-            <div className='priceaction'>
-              <MdAddCircle className={styles.inceriment} size={"3vh"} />
-              <p className={styles.total}>{totalPrice}</p>
-              <FaMinus className={styles.deceriment} size={"3vh"} />
-            </div>
-          </div>
-          <div className={styles.basketDetail}>توضیحات</div>
-        </div>
-        <div className={styles.menuSide}>
+        )}
+        <div
+          className={styles.menuSide}
+          style={{ width:!isMobile? `${ isBasketOpen ? 67 : 90}%`:`${99}%` }}
+        >
           <div className={styles.categoryBox} ref={categoryBoxRef}>
             <IoIosArrowForward
-              className={styles.leftIcon}
+              className={styles.leftDirection}
               onClick={scrollLeft}
+              style={{ left: `${isBasketOpen ? 32 : 5.5}%` }}
             />
             {data &&
               data[0].map((cat, catindex) => (
@@ -80,25 +111,31 @@ const Store: React.FC<Props> = ({ data }) => {
                     alt={cat.name}
                     width={200}
                     height={200}
-                    className={styles.icon}
+                    className={styles.categorimage}
                   />
                   <div className={styles.categoryName}>{cat.name}</div>
                 </div>
               ))}
             <IoIosArrowForward
-              className={styles.rightIcon}
+              className={styles.rightDirection}
               onClick={scrollRight}
+              style={{ right: `${isBasketOpen ? 2 : 5.5}%` }}
             />
           </div>
           <div className={styles.menuSearchBox}>
             <form className={styles.searchBar}>
               <input
+                ref={refs.search as RefObject<HTMLInputElement>}
                 className={styles.searchInput}
                 type='search'
-                value={"جستجو ..."}
+                placeholder={"جستجو ..."}
               />
               <BiSearch className={styles.searchIcon} />
             </form>
+            <FaBasketShopping
+              className={styles.basketIcon}
+              onClick={() => setIsBasketOpen(!isBasketOpen)}
+            />
           </div>
           <div className={styles.producto}>
             {data &&
@@ -124,7 +161,7 @@ const Store: React.FC<Props> = ({ data }) => {
                     </div>
                   )}
                   <p className={styles.producTitle}>{product.title}</p>
-                  <div className='priceaction'>
+                  <div className={"priceaction"}>
                     <div className={styles.controlBox}>
                       <MdAddCircle
                         className={styles.inceriment}
