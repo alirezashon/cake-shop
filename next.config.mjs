18a3b4ae91bc83e3fdace8bdd935ba242231e2fc
alirezashon <alirezafeshki2017@gmail.com>
2024-06-09 11:34:05 +0330

    notz

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   api: {
-    responseLimit: '8mb',
+    responseLimit: '12mb',
   },
 }
 
--- src/Components/Chat/index.tsx
+++ src/Components/Chat/index.tsx
@@ -2,113 +2,112 @@
 
 // components/ChatUI.tsx
 
-import { useEffect, useState } from 'react'
-import { FaRocketchat } from 'react-icons/fa'
-import styles from './index.module.css'
-import { GiCrossMark } from 'react-icons/gi'
+import { useEffect, useState } from "react"
+import { FaRocketchat } from "react-icons/fa"
+import styles from "./index.module.css"
+import { GiCrossMark } from "react-icons/gi"
 
 interface Message {
-	id: string
-	client: string
-	content: string
-	sender: '*u&$e#' | '&a(D^m$n@'
+  id: string
+  client: string
+  content: string
+  sender: "*u&$e#" | "&a(D^m$n@"
 }
 
 const ChatUI = () => {
-	const [showChat, setShowChat] = useState(false)
-	const [messages, setMessages] = useState<Message[]>([])
-	const [newMessage, setNewMessage] = useState('')
-	const [client, setClient] = useState<string>('')
-	// const dummyChatHistory: Message[] = [
-	// 	{ id: 2, content: 'الکی مثلا یه مشکلی داشتم قبلا اینجا پیاماش مونده بوده باشه', sender: 'user' },
-	// 	{ id: 1, content: 'سلام در خدمت که خب آها باشه ردیفه مرسی که ممنون سپاسگذارتیم', sender: 'other' },
-	// ]
-	// setMessages(dummyChatHistory)
+  const [showChat, setShowChat] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+  const [newMessage, setNewMessage] = useState("")
+  const [client, setClient] = useState<string>("")
+  // const dummyChatHistory: Message[] = [
+  // 	{ id: 2, content: 'الکی مثلا یه مشکلی داشتم قبلا اینجا پیاماش مونده بوده باشه', sender: 'user' },
+  // 	{ id: 1, content: 'سلام در خدمت که خب آها باشه ردیفه مرسی که ممنون سپاسگذارتیم', sender: 'other' },
+  // ]
+  // setMessages(dummyChatHistory)
 
-	const fetchChatHistory = async () => {
-		const response = await fetch('/api/chat/GET', {
-			headers: { 'Content-Type': 'application/json' },
-			method: 'POST',
-			body: JSON.stringify({ authType: '&n)E(w*o&n$e^w#q@', client }),
-		})
-		const data = await response.json()
-		data.messages && setMessages(data.messages)
-	}
-	
-	useEffect(() => {
-		!client && setClient(JSON.parse(localStorage.getItem('user') || ''))
-		fetchChatHistory()
-	}, [])
+  const fetchChatHistory = async () => {
+    const response = await fetch("/api/chat/GET", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({ authType: "&n)E(w*o&n$e^w#q@", client }),
+    })
+    const data = await response.json()
+    data.messages && setMessages(data.messages)
+  }
 
-	const toggleChat = () => {
-		setShowChat((prev) => !prev)
-	}
+  useEffect(() => {
+    // const name = JSON.parse(localStorage.getItem("user") || "")
+    const name =`custom user ${Date}` 
+	  !client && setClient(name)
+    fetchChatHistory()
+  }, [])
 
-	const sendMessage = async () => {
-		console.log('messagooo')
-		const response = await fetch('/api/chat', {
-			headers: { 'Content-Type': 'application/json' },
-			method: 'POST',
-			body: JSON.stringify({
-				authType: '&M%e$A#g$e#I%n&Z*',
-				message: newMessage,
-				client: `${client}`,
-			}),
-		})
-		console.log(await response.json())
-		console.log(response.status)
-	}
+  const toggleChat = () => {
+    setShowChat((prev) => !prev)
+  }
 
-	return (
-		<div className={styles.chatContainer}>
-			{!showChat && (
-				<div
-					className={styles.chatIcon}
-					onClick={toggleChat}>
-					<FaRocketchat size={'5vh'} />
-				</div>
-			)}
-			{showChat && (
-				<div className={styles.chatBox}>
-					<div className={styles.header}>
-						<GiCrossMark
-							className={styles.cross}
-							size={'5vh'}
-							onClick={() => setShowChat(false)}
-						/>
-					</div>
-					<div className={styles.messages}>
-						{messages?.map((message) => (
-							<div
-								key={message.id}
-								className={`${styles.message} ${
-									message.sender === '*u&$e#'
-										? styles.userMessage
-										: styles.otherMessage
-								}`}>
-								{message.content}
-							</div>
-						))}
-					</div>
-					<div className={styles.inputContainer}>
-						<button
-							onClick={sendMessage}
-							className={styles.sendButton}>
-							ارسال
-						</button>
-						<input
-							type='text'
-							value={newMessage}
-							onChange={(e) => setNewMessage(e.target.value)}
-							onKeyDown={(e) => e.key === 'Enter' && sendMessage()}
-							placeholder='...ارسال پیام'
-							className={styles.input}
-						/>
-					</div>
-				</div>
-			)}
-		</div>
-	)
+  const sendMessage = async () => {
+    console.log("messagooo")
+    const response = await fetch("/api/chat", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      body: JSON.stringify({
+        authType: "&M%e$A#g$e#I%n&Z*",
+        message: newMessage,
+        client: `${client}`,
+      }),
+    })
+    console.log(await response.json())
+    console.log(response.status)
+  }
+
+  return (
+    <div className={styles.chatContainer}>
+      {!showChat && (
+        <div className={styles.chatIcon} onClick={toggleChat}>
+          <FaRocketchat size={"5vh"} />
+        </div>
+      )}
+      {showChat && (
+        <div className={styles.chatBox}>
+          <div className={styles.header}>
+            <GiCrossMark
+              className={styles.cross}
+              size={"5vh"}
+              onClick={() => setShowChat(false)}
+            />
+          </div>
+          <div className={styles.messages}>
+            {messages?.map((message) => (
+              <div
+                key={message.id}
+                className={`${styles.message} ${
+                  message.sender === "*u&$e#"
+                    ? styles.userMessage
+                    : styles.otherMessage
+                }`}
+              >
+                {message.content}
+              </div>
+            ))}
+          </div>
+          <div className={styles.inputContainer}>
+            <button onClick={sendMessage} className={styles.sendButton}>
+              ارسال
+            </button>
+            <input
+              type='text'
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+              placeholder='...ارسال پیام'
+              className={styles.input}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default ChatUI
